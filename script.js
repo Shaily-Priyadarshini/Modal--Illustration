9be13f2dd7d8a3326a1fbdab7fd526092f3eed38
Shaily Priyadarshini <priyadarshinishaily@gmail.com>
2023-01-27 01:13:44 +0530

    Modal

---
+++ script.js
@@ -0,0 +1,27 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closeBtn = document.querySelector(".btn-close-modal");
+const showModal = document.querySelectorAll(".btn");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener("click", openModal);
+}
+
+closeBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key == "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
